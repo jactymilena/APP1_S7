dbf7e61f3c5d87612aa1c5a1727872848add2368
aliseeperreault <pera3307@usherbrooke.ca>
2024-01-23 13:17:25 -0500

    new folder

--- Algorithme Genetique/main.py
+++ Algorithme Genetique/main.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
     # Niveau 0 - sans obstacle - 'assets/Mazes/mazeMedium_0'
     # Niveau 1 - avec obstacles - 'assets/Mazes/mazeMedium_1'
     # Niveau 2 - avec obstacles, portes et un ennemi - 'assets/Mazes/mazeMedium_2'
-    # Niveau 2 - avec obstacles, portes et plusieurs ennemis - 'assets/Mazes/mazeMedium_3'
+    # Niveau 2 - avec obstacles, portes et plusieurs ennemis - 'assets/Mazes/mazeMedium_2'
 
     theAPP = App('assets/Mazes/mazeMedium_0')
     theAPP.on_execute()
-
