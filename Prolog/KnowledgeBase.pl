c7703a96d61a543ff7bf35f4c9efadeeafee5b64
jacty <saej3101@usherbrooke.ca>
2024-01-24 09:31:39 -0500

    Fixed comments following merge

--- Prolog/KnowledgeBase.pl
+++ Prolog/KnowledgeBase.pl
@@ -31,7 +31,6 @@ longueur([X|Qliste], 0) :- longueur(Qliste, 0).
 
 trois_cristaux([_|Env]) :- longueur(Env, NombreItems), NombreItems =:= 3.
 
-%si 3 cristaux(...)
 
 % action(Env, Res):-
 %     % retirer second
@@ -42,31 +41,31 @@ trois_cristaux([_|Env]) :- longueur(Env, NombreItems), NombreItems =:= 3.
 %     member(blue,Env), plus un blue, retirer dernier blue;
 %     retirer premier.
 
-si 3 cristaux(...)
-action(Env, Res):-
-    member(not(red),Env), print('second');
-    member(blanc,Env), si blanc dernier , print('third');
-    member(blue,Env), plus un blue, retirer dernier blue;
-    print('first').
+% si 3 cristaux(...)
+% action(Env, Res):-
+%     member(not(red),Env), print('second');
+%     member(blanc,Env), si blanc dernier , print('third');
+%     member(blue,Env), plus un blue, retirer dernier blue;
+%     print('first').
 
-# 4 cristaux
-# action(Env, four()):-
-#     member(red), plus un red, member(silver,Env), retirer dernier red;
-#     member(yellow,Env), dernier yellow, member(not(red)), print('first');
-#     member(blue,Env), un seul blue, print('first');
-#     member(yellow,Env), plus un yellow, print('fourth');
-#     print('second').
+% 4 cristaux
+% action(Env, four()):-
+%      member(red), plus un red, member(silver,Env), retirer dernier red;
+%      member(yellow,Env), dernier yellow, member(not(red)), print('first');
+%      member(blue,Env), un seul blue, print('first');
+%      member(yellow,Env), plus un yellow, print('fourth');
+%      print('second').
 
-# 5 cristaux
-# action(Env, five()):-
-#     member(black,Env), dernier black, member(gold,Env), print('fourth');
-#     member(red,Env), un seul red, member(yellow,Env), plus un yellow, print('first');
-#     member(not(black),Env), print('second');
-#     print('first').
+% 5 cristaux
+% action(Env, five()):-
+%     member(black,Env), dernier black, member(gold,Env), print('fourth');
+%     member(red,Env), un seul red, member(yellow,Env), plus un yellow, print('first');
+%     member(not(black),Env), print('second');
+%     print('first').
 
-# 6 cristaux
-# action(Env, six()):-
-#     member(not(yellow),Env), member(bronze), print('third');
-#     member(yellow,Env), un seul yellow, member(blanc,Env), plus un blanc, print('fourth');
-#     member(not(red),Env), print('sixth');
-#     print('fourth').
+% 6 cristaux
+% action(Env, six()):-
+%     member(not(yellow),Env), member(bronze), print('third');
+%     member(yellow,Env), un seul yellow, member(blanc,Env), plus un blanc, print('fourth');
+%     member(not(red),Env), print('sixth');
+%     print('fourth').
