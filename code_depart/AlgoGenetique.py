c704771b52f8b6e258b00a2288dd45347720bc86
aliseeperreault <pera3307@usherbrooke.ca>
2024-01-29 14:39:22 -0500

    Algo Gen - Clean up

--- code_depart/AlgoGenetique.py
+++ code_depart/AlgoGenetique.py
@@ -7,11 +7,8 @@ class AlgoGenetique:
         simulation = genetic.Genetic()
         loser = True
         run = 0
-        run_won = 0
-        gen_higher_than_500 = 0
 
         while loser:
-            loser = True
             run = run + 1
             print(f"RUN : {run}")
 
@@ -33,11 +30,10 @@ class AlgoGenetique:
 
                     if fitness_tuple[0] == 4:
                         loser = False
-                        run_won = run_won + 1
                         simulation.bestIndividual = individual
-
-                        if i > 500:
-                            gen_higher_than_500 = gen_higher_than_500 + 1
+                        # run_won = run_won + 1
+                        # if i > 500:
+                        #     gen_higher_than_500 = gen_higher_than_500 + 1
 
                 if loser:
                     if (i > 300) and (simulation.bestIndividualFitness < 8):
@@ -46,27 +42,26 @@ class AlgoGenetique:
                     simulation.fitness = fitness
                     simulation.eval_fit()
 
-                    if i % 299 == 0:
+                    if i % 300 == 0:
                         simulation.print_progress()
 
                     simulation.new_gen()
                     simulation.decode_individuals()
-                else:
-                    print('Fitness of the winner:')
-                    player.set_attributes(simulation.bestIndividual)
-                    print(monster.mock_fight(player))
-                    break
 
-            #genetic.display_generations(simulation)
-            print('Algo failed:')
-            player.set_attributes(simulation.bestIndividual)
-            print(monster.mock_fight(player))
-            print('')
-            #print('Nombre de run que ça a pris:')
-            #print(run)
+            if loser:
+                print('Algo failed : Fitness of the best individual:')
+                player.set_attributes(simulation.bestIndividual)
+                print(monster.mock_fight(player))
+                print('')
 
+
+        #genetic.display_generations(simulation)
+        print('Fitness of the winner:')
+        player.set_attributes(simulation.bestIndividual)
+        print(monster.mock_fight(player))
+        print('')
+        print('Nombre de run que ça a pris:')
+        print(run)
         print('FIN')
-        print(f"Nombre de silumation réussi: {run_won}")
-        print(f"Nombre de silumation réussi en haut de 500 génération: {gen_higher_than_500}")
 
         return simulation.bestIndividual
